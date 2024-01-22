eedba0935c842d616169b79e8b0b20ea9808998f
Marko Seppänen <marko@marko.xyz>
2024-01-22 17:07:50 +0200

    w1d3 done

--- w1d3_MD3/app/src/main/java/xyz/marko/w1d3_md3/MainActivity.kt
+++ w1d3_MD3/app/src/main/java/xyz/marko/w1d3_md3/MainActivity.kt
@@ -5,15 +5,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,6 +36,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.marko.w1d3_md3.ui.theme.W1d3_MD3Theme
@@ -36,8 +45,57 @@ import xyz.marko.w1d3_md3.ui.theme.W1d3_MD3Theme
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { W1d3_MD3Theme { MainScaffold() } }
+    setContent { W1d3_MD3Theme {  MainScaffold() } }
   }
+}
+
+data class Message(val author: String, val body: String)
+
+object SampleData {
+  // Sample conversation data
+  val conversationSample =
+      listOf(
+          Message("Lexi", "Test...Test...Test..."),
+          Message(
+              "Lexi",
+              """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)"""
+                  .trim()),
+          Message(
+              "Lexi",
+              """I think Kotlin is my favorite programming language.
+            |It's so much fun!"""
+                  .trim()),
+          Message("Lexi", "Searching for alternatives to XML layouts..."),
+          Message(
+              "Lexi",
+              """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)"""
+                  .trim()),
+          Message("Lexi", "It's available from API 21+ :)"),
+          Message(
+              "Lexi",
+              "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"),
+          Message("Lexi", "Android Studio next version's name is Arctic Fox"),
+          Message("Lexi", "Android Studio Arctic Fox tooling for Compose is top notch ^_^"),
+          Message(
+              "Lexi", "I didn't know you can now run the emulator directly from Android Studio"),
+          Message(
+              "Lexi",
+              "Compose Previews are great to check quickly how a composable layout looks like"),
+          Message("Lexi", "Previews are also interactive after enabling the experimental setting"),
+          Message("Lexi", "Have you tried writing build.gradle with KTS?"),
+      )
 }
 
 @Composable
@@ -48,12 +106,14 @@ fun MainScaffold() {
   }
 }
 
+val testMessage = Message("Marko", "Moi vaan")
+
 @Composable
 fun MainContent(paddingValues: PaddingValues) {
   Column(
       horizontalAlignment = Alignment.CenterHorizontally,
       modifier = Modifier.padding(paddingValues).padding(12.dp).fillMaxSize()) {
-        MainCard()
+        Conversation(messages = SampleData.conversationSample)
       }
 }
 
@@ -73,23 +133,47 @@ fun MainTopBar() {
           ),
       title = { Text("Top Bar") },
       navigationIcon = {
-        IconButton(onClick = { /* TODO */}) { Icon(Icons.Filled.ArrowBack, null) }
+        IconButton(onClick = { /* TODO */}) {
+          Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Go back")
+        }
       },
-      actions = { IconButton(onClick = { /* TODO */}) { Icon(Icons.Filled.AccountCircle, null) } })
+      actions = {
+        IconButton(onClick = { /* TODO */}) {
+          Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "Account")
+        }
+      })
 }
 
 @Composable
-fun MainCard() {
-  ElevatedCard(modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
+fun Conversation(messages: List<Message>) {
+  LazyColumn{ items(messages) { message -> MessageCard(message) } }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+  ElevatedCard(modifier = Modifier.fillMaxWidth().wrapContentHeight().padding(8.dp)) {
     Column(modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
       Image(
           painter = painterResource(R.drawable.forest),
           contentScale = ContentScale.FillWidth,
           contentDescription = "Forest with sun rays cast through the canopy.",
           modifier = Modifier.fillMaxWidth())
-      Text(
-          text =
-              "Mieleni minun tekevi, aivoni ajattelevi lähteäni laulamahan, saa'ani sanelemahan, sukuvirttä suoltamahan, lajivirttä laulamahan. Sanat suussani sulavat, puhe'et putoelevat, kielelleni kerkiävät, hampahilleni hajoovat.")
+      Row(
+          horizontalArrangement = Arrangement.SpaceBetween,
+          modifier = Modifier.fillMaxWidth().padding(4.dp)) {
+            Text(text = msg.author, fontWeight = FontWeight.ExtraBold)
+            Text(text = msg.body, textAlign = TextAlign.End)
+          }
+      Row(
+          horizontalArrangement = Arrangement.SpaceBetween,
+          modifier = Modifier.fillMaxWidth().padding(4.dp)) {
+            Button(onClick = { /*TODO*/}) {
+              Icon(imageVector = Icons.Filled.ThumbUp, contentDescription = "Thumbs up.")
+            }
+            Button(onClick = { /*TODO*/}) {
+              Icon(imageVector = Icons.Filled.Star, contentDescription = "Star.")
+            }
+          }
     }
   }
 }
